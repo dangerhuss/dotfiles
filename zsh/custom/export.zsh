4860e4fed3bc6e5fa2e903c6bd3e5f52022a75dc
Andrew Huss <git@ahuss.com>
2017-01-16 21:12:11 -0800

    Added zsh dotfiles

---
+++ zsh/custom/export.zsh
@@ -0,0 +1,12 @@
+export SRC="$HOME/src"
+export DOT="$SRC/dotfiles"
+
+# ZSH
+export ZSH="$SRC/oh-my-zsh"
+export ZSH_CUSTOM="$DOT/zsh/custom"
+
+# GO
+export GOPATH="$HOME/go"
+
+# PATH
+export PATH="$PATH:$GOPATH/bin"
---
+++ zsh/zshrc.zsh
@@ -0,0 +1,8 @@
+ZSH_THEME="robbyrussell"
+UPDATE_ZSH_DAYS=1
+plugins=(git brew zsh-syntax-highlighting)
+
+export ZSH_CUSTOM="$HOME/src/dotfiles/zsh/custom"
+export ZSH="$HOME/src/oh-my-zsh"
+source "$ZSH/oh-my-zsh.sh"
+
